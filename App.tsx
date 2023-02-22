528abd42f75a2498bb21a26cb82f064235534ebd
Shaikh Afroz <shaikhafroz@geekyants.com>
2023-02-22 15:34:56 +0530

    FIX: removed react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {Appbar} from 'react-native-paper';
-// import {Appbar} from 'react-native-paper';
 
 const MyApp = () => {
   return (
     <>
-      <Appbar>
+      {/* <Appbar>
         <Appbar.Content title="Mobile Pipeline Test" />
-      </Appbar>
+      </Appbar> */}
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Version 2.1</Text>
         <Text>- Circle CI ✅</Text>
