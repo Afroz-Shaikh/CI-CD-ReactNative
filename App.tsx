4e5002a3009b7b5d77dd21674d9b8585143b2d35
Shaikh Afroz <shaikhafroz@geekyants.com>
2023-02-22 15:27:39 +0530

    ADD: Changes to 2.0

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {Appbar} from 'react-native-paper';
+// import {Appbar} from 'react-native-paper';
 
 const MyApp = () => {
   return (
     <>
-      <Appbar.Header>
+      <Appbar>
         <Appbar.Content title="Mobile Pipeline Test" />
-      </Appbar.Header>
+      </Appbar>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Version 2.1</Text>
         <Text>- Circle CI ✅</Text>
